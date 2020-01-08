6608dc8d90174d3dd17076927b78ff7bd5a601f3
Scott Idler <scott.a.idler@gmail.com>
2020-01-08 08:21:01 -0800

    added pipupgrade (pu) and retabbed to spaces

--- shell-functions.sh
+++ shell-functions.sh
@@ -7,22 +7,22 @@ fi
 
 VIM="`print -r -- =vim`"
 function vim() {
-	ARGS=("$@")
+    ARGS=("$@")
 
-	VIM_MIN="${VIM_MIN:-2}"
-	VIM_MAX="${VIM_MAX:-3}"
+    VIM_MIN="${VIM_MIN:-2}"
+    VIM_MAX="${VIM_MAX:-3}"
 
-	for arg in "${ARGS[@]}"; do
-		if [[ "$arg" == "-"* ]]; then
-			eval $VIM "${ARGS[@]}"
-		fi
-	done
+    for arg in "${ARGS[@]}"; do
+        if [[ "$arg" == "-"* ]]; then
+            eval $VIM "${ARGS[@]}"
+        fi
+    done
 
-	case ${#ARGS[@]} in
-		[0-1])                  eval $VIM    "${ARGS[@]}";;
-		[$VIM_MIN-$VIM_MAX])    eval $VIM -O "${ARGS[@]}";;
-		*)                      eval $VIM -o "${ARGS[@]}";;
-	esac
+    case ${#ARGS[@]} in
+        [0-1])                  eval $VIM    "${ARGS[@]}";;
+        [$VIM_MIN-$VIM_MAX])    eval $VIM -O "${ARGS[@]}";;
+        *)                      eval $VIM -o "${ARGS[@]}";;
+    esac
 }
 
 function vim-which() {
@@ -54,31 +54,31 @@ function touch755() {
 }
 
 function follow() {
-	FILE="$1"
-	printf $FILE
-	while true; do
-		FILE=`readlink $FILE`
-		if [ -z "$FILE" ]; then
-			break
-		fi
-		printf " -> $FILE"
-	done
-	echo
+    FILE="$1"
+    printf $FILE
+    while true; do
+        FILE=`readlink $FILE`
+        if [ -z "$FILE" ]; then
+            break
+        fi
+        printf " -> $FILE"
+    done
+    echo
 }
 
 function dbsl() {
-	DIR="${1:-`pwd`}"
-	RESULTS=`find -L $DIR -maxdepth 1 -type l`
-	if [ -n "$RESULTS" ]; then
-		echo "[d]elete [b]roken [s]ymbolic [l]inks"
-		echo "$RESULTS"
-		echo "-> /dev/null"
-		find -L $DIR -maxdepth 1 -type l -delete
-	fi
+    DIR="${1:-`pwd`}"
+    RESULTS=`find -L $DIR -maxdepth 1 -type l`
+    if [ -n "$RESULTS" ]; then
+        echo "[d]elete [b]roken [s]ymbolic [l]inks"
+        echo "$RESULTS"
+        echo "-> /dev/null"
+        find -L $DIR -maxdepth 1 -type l -delete
+    fi
 }
 
 function mine() {
-	sudo chown -R $USER:$USER "$@"
+    sudo chown -R $USER:$USER "$@"
 }
 
 function yours() {
@@ -90,108 +90,108 @@ function ours() {
 }
 
 function mkfiles() {
-	NUM="$1"
-	DIR="${2:-.}"
+    NUM="$1"
+    DIR="${2:-.}"
 
-	echo "NUM=$NUM"
-	echo "DIR=$DIR"
+    echo "NUM=$NUM"
+    echo "DIR=$DIR"
 
-	if ((NUM > 0)); then
-		mkdir -p $DIR
-		for i in `seq 1 $NUM`; do
-			mktemp -p $DIR
-		done
-	else
-		echo "NUM [DIR]"
-		return 1
-	fi
+    if ((NUM > 0)); then
+        mkdir -p $DIR
+        for i in `seq 1 $NUM`; do
+            mktemp -p $DIR
+        done
+    else
+        echo "NUM [DIR]"
+        return 1
+    fi
 }
 
 function ls-docker-tags() {
-	if [ "$#" -ne 1 ]; then
-		echo "supply image name"
-		return 1
-	fi
-	wget -q https://registry.hub.docker.com/v1/repositories/$1/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
+    if [ "$#" -ne 1 ]; then
+        echo "supply image name"
+        return 1
+    fi
+    wget -q https://registry.hub.docker.com/v1/repositories/$1/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
 
 }
 
 function dhclient-restart() {
-	ETHDEV=${1:-$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')}
-	sudo dhclient -r -v $ETHDEV && sudo rm /var/lib/dhcp/dhclient.*; sudo dhclient -v $ETHDEV
+    ETHDEV=${1:-$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')}
+    sudo dhclient -r -v $ETHDEV && sudo rm /var/lib/dhcp/dhclient.*; sudo dhclient -v $ETHDEV
 }
 
 function pmu() {
-	if hash apt-get 2> /dev/null; then
-		echo "updating apt packages..."
-		sudo apt-get update
-		sudo apt-get upgrade -y --allow-unauthenticated --allow-downgrades
-		sudo apt-get dist-upgrade -y
-		sudo apt-get autoremove -y
-	fi
-	if hash dnf 2> /dev/null; then
-		echo "updating dnf packages..."
-		sudo dnf upgrade -y
-	elif hash yum 2> /dev/null; then
-		echo "updating yum packages..."
-		sudo yum upgrade -y
-	fi
+    if hash apt-get 2> /dev/null; then
+        echo "updating apt packages..."
+        sudo apt-get update
+        sudo apt-get upgrade -y --allow-unauthenticated --allow-downgrades
+        sudo apt-get dist-upgrade -y
+        sudo apt-get autoremove -y
+    fi
+    if hash dnf 2> /dev/null; then
+        echo "updating dnf packages..."
+        sudo dnf upgrade -y
+    elif hash yum 2> /dev/null; then
+        echo "updating yum packages..."
+        sudo yum upgrade -y
+    fi
 }
 
-function piu3() {
-	if hash pip3 2> /dev/null; then
-		echo "updating pip3 packages..."
-        pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install --upgrade
-	fi
+function pu() {
+    if hash pipupgrade 2> /dev/null; then
+        echo "updating pip3 packages..."
+        sudo pipupgrade -y
+    fi
 }
 
 function update() {
-	echo
-	pmu
-	echo
-	piu3
+    echo
+    pmu
+    echo
+    pu
 }
 
 function reboot() {
-	kill-chrome
-	kill-firefox
-	sudo systemctl reboot
+    kill-chrome
+    kill-firefox
+    sudo systemctl reboot
 }
 
 function nightly() {
-	URLS=""
-	for ARG in "$@"; do
-		if [ -f "$ARG" ]; then
-			URLS+="$(cat "$ARG" | xargs echo -n) "
-		else
-			URLS+="$ARG "
-		fi
-	done
-	firefox-trunk -private "$URLS"
+    URLS=""
+    for ARG in "$@"; do
+        if [ -f "$ARG" ]; then
+            URLS+="$(cat "$ARG" | xargs echo -n) "
+        else
+            URLS+="$ARG "
+        fi
+    done
+    firefox-trunk -private "$URLS"
 }
 
 function replace() {
-	FIND="$1"
-	REPL="$2"
-	DIR="${3-.}"
-	if [ -z "$FIND" ]; then
-		echo "replace FIND [REPL] [DIR]"
-		return 1
-	fi
-	find "$DIR" -path ./.git -prune -o -type f -exec sed -i "s/$FIND/$REPL/g" {} \;
+    FIND="$1"
+    REPL="$2"
+    DIR="${3-.}"
+    if [ -z "$FIND" ]; then
+        echo "replace FIND [REPL] [DIR]"
+        return 1
+    fi
+    find "$DIR" -path ./.git -prune -o -type f -exec sed -i "s/$FIND/$REPL/g" {} \;
 }
 
 function ovpn() {
-	ARGS="$@"
-	sudo pkill openvpn
-	find ~/.openvpn/ -name *.ovpn |
-	while read FILEPATH; do
-		DIR=`dirname $FILEPATH`
-		CFG=`basename $FILEPATH`
-		CMD="(cd $DIR && sudo openvpn --config $CFG $ARGS) &"
-		echo "$CMD"
-		eval "$CMD"
-	done
+    ARGS="$@"
+    sudo pkill openvpn
+    find ~/.openvpn/ -name *.ovpn |
+    while read FILEPATH; do
+        DIR=`dirname $FILEPATH`
+        CFG=`basename $FILEPATH`
+        CMD="(cd $DIR && sudo openvpn --config $CFG $ARGS) &"
+        echo "$CMD"
+        eval "$CMD"
+    done
 }
 
 function sls() {
