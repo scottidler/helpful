206b6552ababd4cf6745bce4dc2a0c2749fc3907
Scott Idler <scott.a.idler@gmail.com>
2020-07-22 12:58:06 -0700

    added back function to ls-ppas

--- shell-functions.sh
+++ shell-functions.sh
@@ -25,6 +25,17 @@ function vim() {
     esac
 }
 
+# listppa Script to get all the PPA installed on a system ready to share for reininstall
+function ls-ppas() {
+    for APT in `find /etc/apt/ -name \*.list`; do
+        grep -o "^deb http://ppa.launchpad.net/[a-z0-9\-]\+/[a-z0-9\-]\+" $APT | while read ENTRY ; do
+            USER=`echo $ENTRY | cut -d/ -f4`
+            PPA=`echo $ENTRY | cut -d/ -f5`
+            echo ppa:$USER/$PPA
+        done
+    done
+}
+
 function vim-which() {
     WHICHD=()
     for ARG in "$@"; do
